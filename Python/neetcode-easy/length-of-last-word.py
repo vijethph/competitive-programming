f50110a5164294439c7c7c065e22ba1dd870804f
Vijeth P H <vijeth.ph@outlook.com>
2024-05-12 18:33:56 +0530

    fix(solution); update solution for a problem

--- Python/neetcode-easy/length-of-last-word.py
+++ Python/neetcode-easy/length-of-last-word.py
@@ -9,7 +9,8 @@ class Solution:
         # i, length = len(s) - 1, 0
 
         # while s[i] == " ":
-        #     i-= 1
+        #     i -= 1
         # while i >= 0 and s[i] != " ":
         #     length += 1
+        #     i -= 1
         # return length
