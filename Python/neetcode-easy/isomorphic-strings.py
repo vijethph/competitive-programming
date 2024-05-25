2d610b82b9f93027fce2dd9a1b69cb22917e9016
Vijeth P H <vijeth.ph@outlook.com>
2024-05-25 20:55:54 +0530

    feat(code): add solutions for some python problems - 21

--- Python/neetcode-easy/isomorphic-strings.py
+++ Python/neetcode-easy/isomorphic-strings.py
@@ -11,3 +11,13 @@ class Solution:
             s2t[s[i]] = t[i]
             t2s[t[i]] = s[i]
         return True
+
+        # alternative solution:
+        mapST, mapTS = {}, {}
+        for c1, c2 in zip(s,t):
+
+            if ((c1 in mapST and mapST[c1] != c2) or (c2 in mapTS and mapTS[c2] != c1)):
+                return False
+            mapST[c1] = c2
+            mapTS[c2] = c1 
+        return True
