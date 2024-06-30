daa6c6d25bb60809cb76c563dfd47aea32d52915
Vijeth P H <vijeth.ph@outlook.com>
2024-06-30 20:46:25 +0530

    feat(code): add solutions for some python problems - 32

---
+++ Python/neetcode-easy/next-greater-element-1.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/next-greater-element-i/description/
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # O(n * m)
+        nums1index = { n:i for i, n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+
+        for i in range(len(nums2)):
+            if nums2[i] not in nums1index:
+                continue
+
+            for j in range(i + 1, len(nums2)):
+                if nums2[j] > nums2[i]:
+                    idx = nums1index[nums2[i]]
+                    res[idx] = nums2[j]
+                    break
+        
+        return res
+
+        # alternative solution
+        nums1index = { n:i for i, n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+
+        stack = []
+
+        for i in range(len(nums2)):
+            cur = nums2[i]
+
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = nums1index[val]
+                res[idx] = cur
+            
+            if cur in nums1index:
+                stack.append(cur)
+        
+        return res
