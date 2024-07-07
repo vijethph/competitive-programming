813aab5c0228019c48b2fed0417f3689c21e7191
Vijeth P H <vijeth.ph@outlook.com>
2024-07-07 10:10:44 +0530

    feat(code): add solutions for some python problems - 34

---
+++ Python/neetcode-easy/shift-2d-grid.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/shift-2d-grid/description/
+
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        col, nums = len(grid[0]), sum(grid, [])
+        k = k % len(nums)
+        nums = nums[-k:] + nums[:-k]
+        return [nums[i:i+col] for i in range(0, len(nums), col)]
