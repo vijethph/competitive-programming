1d4d88a161fe3c13f01082e6d89418d49f0dd8d7
Vijeth P H <vijeth.ph@outlook.com>
2024-07-10 20:34:14 +0530

    feat(code): add solutions for some python problems - 36

---
+++ Python/neetcode-medium/longest-substring-without-repeating-characters.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # use set to store unique substring, and a sliding window to shift between characters
+        charSet = set()
+
+        left = 0
+        res = 0
+
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            charSet.add(s[right])
+            res = max(res, right - left + 1)
+        
+        return res
