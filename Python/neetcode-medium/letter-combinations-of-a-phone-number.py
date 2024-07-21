d2431445eee64d5d3414415ebf1c5f2095ec884c
Vijeth P H <vijeth.ph@outlook.com>
2024-07-21 11:14:00 +0530

    feat(code): add solutions for some python problems - 41

---
+++ Python/neetcode-medium/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,29 @@
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        def backtrack(idx, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+
+            for c in digitToChar[digits[idx]]:
+                backtrack(idx + 1, curStr + c)
+        
+        if digits:
+            backtrack(0, "")
+        
+        return res
---
+++ Python/neetcode-medium/rotate-image.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/rotate-image/description/
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        left, right = 0, len(matrix) - 1
+
+        while left < right:
+            for i in range(right - left):
+                top, bottom = left, right
+                
+                # save the top left value
+                topLeft = matrix[top][left + i]
+
+                # move bottom left into top left
+                matrix[top][left + i] = matrix[bottom - i][left]
+
+                # move bottom right into bottom left
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+
+                # move top right into bottom right
+                matrix[bottom][right - i] = matrix[top + i][right]
+
+                # move top left into top right
+                matrix[top + i][right] = topLeft
+            
+            right -= 1
+            left += 1
