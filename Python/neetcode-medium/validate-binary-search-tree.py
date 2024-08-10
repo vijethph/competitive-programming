81463c8bdae30e337fad44ba1f0d21cf1cc1e9d0
Vijeth P H <vijeth.ph@outlook.com>
2024-08-10 20:10:00 +0530

    feat(code): add solutions for some python problems - 46

---
+++ Python/neetcode-medium/validate-binary-search-tree.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/validate-binary-search-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        prev = float('-inf')
+        def inorder(node):
+            nonlocal prev
+            if not node:
+                return True
+            if not (inorder(node.left) and prev < node.val):
+                return False
+            prev = node.val
+            return inorder(node.right)
+        return inorder(root)
