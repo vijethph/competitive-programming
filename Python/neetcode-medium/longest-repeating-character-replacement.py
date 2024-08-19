18fc9603a08326ae7fb320e39ce280ae82c0f3e6
Vijeth P H <vijeth.ph@outlook.com>
2024-08-19 13:00:39 +0530

    feat(code): add solutions for some python problems - 48

---
+++ Python/neetcode-medium/longest-repeating-character-replacement.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/longest-repeating-character-replacement/
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # sliding window length - count of most frequent character in that window <= k
+        count = {}
+        res = 0
+
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+
+            res = max(res, r - l + 1)
+        
+        return res
