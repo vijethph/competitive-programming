d89e7aca4a219489ba719391024863e9920fe07d
Vijeth P H <vijeth.ph@outlook.com>
2025-02-23 21:31:41 +0530

    feat(code): add solutions to linked list problems - 1

---
+++ Python/Algorithms2/LinkedList/Problems/reorder-list.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/reorder-list/description/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return
+
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second = slow.next
+        prev = slow.next = None # split the list into 2 halves
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        # merge two halfs
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
