dd981d3cd60b732238bba3a3d3089c8d2e22ef62
Vijeth P H <vijeth.ph@outlook.com>
2025-04-27 10:23:31 +0530

    feat(code): add solutions to dp problems - 6

---
+++ Python/Algorithms2/DynamicProgramming/Problems/longest-increasing-subsequence.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # time: O(n^2)
+        # start at end and compute increasing subsequence
+        LIS = [1] * len(nums) # consider the element itself
+
+        for i in range(len(nums) -1, -1, -1): # reverse
+            for j in range(i+1, len(nums)): # look forward
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        
+        return max(LIS)
+
