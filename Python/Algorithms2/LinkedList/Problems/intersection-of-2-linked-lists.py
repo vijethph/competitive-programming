a34974e7c931263c4ea8fcbf2bad835a05a9f3c3
Vijeth P H <vijeth.ph@outlook.com>
2025-05-12 21:10:14 +0530

    feat(code): add solutions to linked list problems - 10

---
+++ Python/Algorithms2/LinkedList/Problems/intersection-of-2-linked-lists.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/intersection-of-two-linked-lists/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if headA is None or headB is None:
+            return None
+
+        pa = headA # 2 pointers
+        pb = headB
+
+        while pa is not pb:
+            # if either pointer hits the end, switch head and continue the second traversal, 
+            # if not hit the end, just move on to next
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+
+        return pa
+
+        # alternative solution
+        l1, l2 = headA, headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        
+        return l1
---
+++ Python/Algorithms2/LinkedList/Problems/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        temp = head
+        while (temp and temp.next):
+            if temp.next.val == temp.val:
+                temp.next = temp.next.next
+                continue
+            temp = temp.next
+        return head
+
+        # alternative solution:
+        cur = head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next
+
+            cur = cur.next 
+
+        return head
