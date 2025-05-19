0725931d5c0ebaf37ddd18256c86aff8e4b976d9
Vijeth P H <vijeth.ph@outlook.com>
2025-05-19 19:52:00 +0530

    feat(code): add solutions to dp problems - 10

---
+++ Python/Algorithms2/DynamicProgramming/Problems/jump-game-2.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/jump-game-ii/description/
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        res = 0
+        left = right = 0
+
+        while right < len(nums) - 1:
+            farthest = 0
+            for i in range(left, right + 1):
+                farthest = max(farthest, i + nums[i])
+            left = right + 1
+            right = farthest
+
+            res += 1
+        return res
