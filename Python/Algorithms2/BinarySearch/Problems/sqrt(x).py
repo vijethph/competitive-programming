725bcd92ecb35c8ed9e76cc6a3368364be3499d2
Vijeth P H <vijeth.ph@outlook.com>
2025-05-26 21:01:42 +0530

    feat(code): add solutions to binary tree problems - 8

---
+++ Python/Algorithms2/BinarySearch/Problems/sqrt(x).py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/sqrtx/description/
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x
+        res = 0
+
+        while left <= right:
+            mid = left + ((right - left) // 2)
+            if mid**2 > x:
+                right = mid - 1
+            elif mid**2 < x:
+                left = mid + 1
+                res = mid
+            else:
+                return mid
+        
+        return res
