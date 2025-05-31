a3cf4ba472ebb167c7805f15db7765f984581690
Vijeth P H <vijeth.ph@outlook.com>
2025-05-31 12:44:00 +0530

    feat(code): add solutions to graph problems - 8

---
+++ Python/Algorithms2/Graphs/Problems/minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/description/
+
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        adj = defaultdict(list) # node -> list of (neighbor, dist)
+
+        for src, dst, dist in roads:
+            adj[src].append((dst, dist))
+            adj[dst].append((src, dist))
+        
+        def dfs(i):
+            if i in visit:
+                return
+            visit.add(i)
+            nonlocal res
+            for nei, dist in adj[i]:
+                res = min(res, dist)
+                dfs(nei)
+
+        res = float("inf")
+        visit = set()
+        dfs(1)
+        return res
