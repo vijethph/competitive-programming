7026df96fe7eddc47f66b67ef8046a4c0c969039
Vijeth P H <vijeth.ph@outlook.com>
2025-06-01 20:13:58 +0530

    feat(code): add solutions to binary search problems - 10

---
+++ Python/Algorithms2/BinarySearch/Problems/find-peak-element.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/find-peak-element/description/
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + ((right - left) // 2)
+
+            # left neighbor greater
+            if mid > 0 and nums[mid] < nums[mid - 1]:
+                right = mid - 1
+            # right neighbor greater
+            elif mid < len(nums) - 1 and nums[mid] < nums[mid + 1]:
+                left = mid + 1
+            else:
+                return mid
+        
+        
