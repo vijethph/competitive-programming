de51b85139b0dc206fede47da6f6851215e237c6
Vijeth P H <vijeth.ph@outlook.com>
2025-06-07 09:27:57 +0530

    feat(code): add solutions to graph problems - 9

---
+++ Python/Algorithms2/BinarySearch/Problems/single-element-in-a-sorted-array.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + ((right - left) // 2)
+            if ((mid - 1 < 0 or nums[mid - 1] != nums[mid]) and (mid + 1 == len(nums) or nums[mid] != nums[mid + 1])):
+                return nums[mid]
+            
+            leftSize = mid - 1 if nums[mid - 1] == nums[mid] else mid
+            if leftSize % 2:
+                right = mid - 1
+            else:
+                left = mid + 1
