2714dd7d8771b1c75bbd705daa57da04496b9f1d
Vijeth P H <vijeth.ph@outlook.com>
2025-06-26 20:15:36 +0530

    feat(code): add solutions to graph problems - 13

---
+++ Python/Algorithms2/Graphs/Problems/evaluate-division.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/evaluate-division/description/
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj = collections.defaultdict(list) # Map a -> list of [b, a/b]
+        for i, eq in enumerate(equations):
+            a, b = eq
+            adj[a].append([b, values[i]])
+            adj[b].append([a, 1 / values[i]])
+        
+        def bfs(src, target):
+            if src not in adj or target not in adj:
+                return -1
+            q, visit = deque(), set()
+            q.append([src, 1])
+            visit.add(src)
+            while q:
+                n, w = q.popleft()
+                if n == target:
+                    return w
+                for nei, weight in adj[n]:
+                    if nei not in visit:
+                        q.append([nei, w * weight])
+                        visit.add(nei)
+            return -1
+
+        
+        return [bfs(q[0], q[1]) for q in queries]
